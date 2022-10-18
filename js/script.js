f8fd3edca6fdd8a08b57d30ac7f8ba4a5ad279d4
sophie-nguyen-0 <113055711+sophie-nguyen-0@users.noreply.github.com>
2022-10-18 18:26:01 +0000

    js and html

--- js/script.js
+++ js/script.js
@@ -3,3 +3,26 @@
 // This file contains the JS functions for index.html
 
 "use strict"
+
+/**
+ * This function calculates area and perimeter of rectangle.
+ */
+function myButtonClicked() {
+  //input
+  const hours = parseFloat(document.getElementById("hours-worked").value)
+  const rate = parseFloat(document.getElementById("hourly-rate").value)
+  const cats = parseInt(document.getElementById("cats-owned").value)
+
+  //process
+  const TAX_RATE = 0.18
+  const payment = (hours * rate) * (1.00 - TAX_RATE)
+  const taxes = (hours * rate) * TAX_RATE
+
+  //output
+  document.getElementById("pay").innerHTML = `Your pay will be: $ ${payment.toFixed(2)}`
+  document.getElementById("tax").innerHTML = `The government will take: $ ${taxes.toFixed(2)}`
+
+  if (cats < 5){
+    
+  }
+}
